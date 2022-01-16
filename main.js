7fd2df46fde99aa13f8f25f279d8b80e893fa1a7
RanuRanjan <ranuranjan25@gmail.com>
2022-01-16 19:32:14 +0530

    funcnality complted

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
+
+// get all tags capture
 let counterStore=document.querySelector("#counter")
-
-
-
-
-
 let increment=document.querySelector("#Inc").addEventListener('click',increase)
 let decrement=document.querySelector("#Dec").addEventListener('click',decrease)
 let reset=document.querySelector("#reset").addEventListener('click',resetCount)
 // console.log(increment);
 // console.log(decrement);
 
-
+// get the value of count from local storage
 let counter=(localStorage.getItem("countValue"));
+
+// appending couter to the html
+counterStore.textContent=counter;
+
+
+
 function increase(){
      counter++
      localStorage.setItem("countValue",counter)
@@ -31,4 +34,3 @@ function resetCount(){
     counterStore.textContent=counter
 }
 // console.log(counter);
-counterStore.textContent=counter;
